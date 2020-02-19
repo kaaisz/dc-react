ab338ba5989a60b5dffa47222b236120826169f6
Kaai Suzuki <suzydp@me.com>
2020-02-19 00:13:52 -0800

    basic-styling-with-css - finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,19 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <div className="Hero">
+        <div className="HeroGroup">
+          <h1>Hi people</h1>
+          <p>Welcome to your new Gatsby site.</p>
+          <p>Now go build something great.</p>
+          {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+            <Image />
+          </div> */}
+          <Link to="/page-2/">Watch the Video</Link>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
