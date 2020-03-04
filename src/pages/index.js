9716da33a1a731f7060be17ab3d9cb9eafba2f7b
Kaai Suzuki <suzydp@me.com>
2020-03-03 23:20:58 -0800

    react-static-data-json - create cell component

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import Card from "../components/Card";
 import Section from '../components/Section';
 import Wave from "../components/wave";
 import staticdata from "../../staticdata.json";
+import Cell from "../components/Cell";
 
 const IndexPage = () => (
   <Layout>
@@ -60,7 +61,8 @@ const IndexPage = () => (
           text="Learn how to build a modern site using React and site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
         />
         {staticdata.cells.map(cell => (
-          <div>{cell.title}</div>
+          <Cell title={cell.title} 
+                image={cell.image} />
         ))}
       </div>
     </div>
