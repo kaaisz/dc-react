4182bb8995f3a78cb8cef621df4cef1f09add8aa
Kaai Suzuki <suzydp@me.com>
2020-03-03 22:36:52 -0800

    react-styled-components - applied styled components with media query

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,20 +2,57 @@ import React from 'react'
 import styled from 'styled-components';
 
 const SectionGroup = styled.div`
-    background: black;
+    /* apply props.image from the function below */
+    background: url(${props => props.image});
     height: 720px;
+    background-size: cover;
+    display: grid;
+    grid-template-rows: 300px auto;
+    grid-gap: 20px;
 `
 
 const SectionLogo = styled.img`
+    align-self: end;
+    width: 128px;
+    margin: 0 auto;
 `
 
 const SectionTitleGroup = styled.div`
+    display: grid;
+    grid-template-columns: 300px auto;
+    max-width: 80%;
+    margin: 0 auto;
+    grid-gap: 20px;
+
+    @media (max-width: 1024px) {
+        max-width: 100%;
+        margin: 0 40px;
+    }
+
+    @media (max-width: 768px) {
+        grid-template-columns: 1fr;
+    }
 `
 
 const SectionTitle = styled.h3`
+    color: white;
+    font-size: 60px;
+    margin: 0;
+    line-height: 1.2;
+
+    @media (max-width: 768px) {
+        font-size: 48px;
+        align-self: center;
+    }
 `
 
 const SectionText = styled.p`
+    color: white;
+
+    @media (max-width: 768px) {
+        align-self: end;
+        margin-top: 0;
+    }
 `
 
 const Section = props => (
